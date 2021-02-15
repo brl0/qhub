e8480a4113b5de99185dcbff9f7a14c666e56e56
Brian Larsen <B_R_L@hotmail.com>
2021-02-15 12:37:29 -0600

    Update modules repo ref

--- qhub/template/{{ cookiecutter.repo_directory }}/infrastructure/kind.tf
+++ qhub/template/{{ cookiecutter.repo_directory }}/infrastructure/kind.tf
@@ -1,6 +1,6 @@
 provider "kind" {}
 
 module "kubernetes" {
-  source = "github.com/brl0/qhub-terraform-modules//modules/kind/kubernetes?ref=local_kind_merge_090"
+  source = "github.com/brl0/qhub-terraform-modules//modules/kind/kubernetes?ref=local_kind_jhub_0111"
   name   = local.cluster_name
 }
